78060751fbaa2fff50c53c60f5a974e942b5356e
amanmaurya005 <amaurya9465@gmail.com>
2025-05-22 16:13:49 +0530

    updated

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
-    let button=document.querySelector('#btn1')
+    let button=document.querySelector('#btn1');
+    let inp=document.querySelectorAll(".inp");
     button.addEventListener('click',()=> {
         let parent = document.querySelector('#head');
         let clone = parent.cloneNode(true);
-
-        let result = document.getElementById('result');
-        result.append(clone);
+ let newinp = clone.querySelectorAll("input, select");
+    newinp.forEach(input => {
+        if (input.tagName === "SELECT") {
+            input.selectedIndex = 0;
+        } else {
+            input.value = "";
+        }
     });
-   
+
+        document.getElementById("result").appendChild(clone)
+        
+    });
